cc4428e89d1392ed87b64e9e8ddc2005f8b909b5
muttakin1991 <shawonmuttakin@gmail.com>
2022-11-24 01:59:11 +0600

    firebase done

--- src/Pages/Layouts/DashboardLayout.js
+++ src/Pages/Layouts/DashboardLayout.js
@@ -12,11 +12,11 @@ const DashboardLayout = () => {
                 <div className="row">
                     <div className="col-3 primary-bg aside">
                         <ul className='mt-lg-4'>
-                            <li className='pt-2 pb-2'><Link to='/dashboard'>dashboard</Link></li>
-                            <li className='pt-3 pb-2'><Link to='/dashboard'>dashboard</Link></li>
-                            <li className='pt-3 pb-2'><Link to='/dashboard'>dashboard</Link></li>
-                            <li className='pt-3 pb-2'><Link to='/dashboard'>dashboard</Link></li>
-                            <li className='pt-3 pb-2'><Link to='/dashboard'>dashboard</Link></li>
+                            <li className='pt-2 pb-2 dashboard-li'><Link to='/dashboard'>dashboard</Link></li>
+                            <li className='pt-3 pb-2 dashboard-li'><Link to='/dashboard'>dashboard</Link></li>
+                            <li className='pt-3 pb-2 dashboard-li'><Link to='/dashboard'>dashboard</Link></li>
+                            <li className='pt-3 pb-2 dashboard-li'><Link to='/dashboard'>dashboard</Link></li>
+                            <li className='pt-3 pb-2 dashboard-li'><Link to='/dashboard'>dashboard</Link></li>
                             
                         </ul>
                     </div>
